c1e2198a3b2eb3f1b1fb03785d32fed8009957b8
JTC17-94 <jc.torres.castillo@gmail.com>
2024-12-05 19:25:17 -0500

    26-11-2024

--- create_database/created_database.sql
+++ create_database/created_database.sql
@@ -1,2 +1,33 @@
 CREATE DATABASE dsrp_prestamos_financieros_6;
 GO
+
+USE dsrp_prestamos_financieros_6;
+GO
+
+-- PERSONAS NATURALES
+CREATE TABLE personas_naturales(
+id INT PRIMARY KEY IDENTITY(1,1),
+numero_documento VARCHAR (20) UNIQUE NOT NULL,
+nombres VARCHAR(255) NOT NULL,
+apellido_paterno VARCHAR(255) NOT NULL,
+apellido_materno VARCHAR(255) NOT NULL,
+email VARCHAR(100) NOT NULL,
+celular VARCHAR(20) NOT NULL,
+direccion VARCHAR(255) NOT NULL
+);
+GO
+
+SELECT*FROM personas_naturales naturales
+
+EXEC sp_help personas_naturales;
+
+
+CREATE TABLE personas_juridicas(
+id INT PRIMARY KEY IDENTITY(1,1),
+numero_documento VARCHAR (20) UNIQUE NOT NULL,
+razon_social VARCHAR(255) NOT NULL,
+email VARCHAR(100) NOT NULL,
+celular VARCHAR(20) NOT NULL,
+direccion VARCHAR(255) NOT NULL
+);
+GO
